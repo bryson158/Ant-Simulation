800af6fbed87326e63f5bc064077f59832c816e1
bryson <bryson158>
2021-05-05 14:34:22 -0400

    first

---
+++ Ant.java
@@ -0,0 +1,3 @@
+public class Ant extends Colony{
+    //This should maybe be a scale of the ants and not the total number of ants for performance.
+}
---
+++ Colony.java
@@ -0,0 +1,6 @@
+public class Colony {
+    //Colony class for the ants
+    String typeOfAnt;
+    //If the ant species is polygynic then this will be true.
+    Boolean polygyne;
+}
